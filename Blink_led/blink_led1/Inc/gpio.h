ceda69cac167f24e38907a7914d991363b57483c
KAVER Devaiah <122006423+kaver06@users.noreply.github.com>
2025-02-04 22:32:33 +0530

    Add files via upload

---
+++ Blink_led/blink_led1/Inc/gpio.h
@@ -0,0 +1,27 @@
+/*
+ * gpio.h
+ *
+ *  Created on: Feb 4, 2025
+ *      Author: Admin
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "base.h"
+
+#define GPIOB_MODER_OFFSET		(0x00)
+#define GPIOB_MODER				(*(volatile unsigned int *)(GPIOB_PHERIPHERAL + GPIOB_MODER_OFFSET ))
+
+#define GPIOB_ODR_OFFSET		(0x14)
+#define GPIOB_ODR				(*(volatile unsigned int *)(GPIOB_PHERIPHERAL + GPIOB_ODR_OFFSET ))
+
+#define RCC_AHB1ENR_OFFSET		(0x30)
+#define RCC_AHB1ENR				(*(volatile unsigned int *)(RCC_PERIPHERAL + RCC_AHB1ENR_OFFSET ))
+
+
+void gpio_init(void);
+void red_led_blink(void);
+
+
+#endif /* GPIO_H_ */
---
+++ Blink_led/blink_led1/Src/main.c
@@ -0,0 +1,11 @@
+#include "gpio.h"
+int main(void)
+{
+    /* Loop forever */
+
+	void gpio_init();
+	for(;;)
+	{
+		void red_led_blink();
+	}
+}
