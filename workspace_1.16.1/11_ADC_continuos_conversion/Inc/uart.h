c2bdb28302b756b2a6b8b9c7acac4df63509cd87
Kaver Devaiah <122006423+kaver06@users.noreply.github.com>
2025-07-16 00:30:46 +0530

    Add files via upload

---
+++ workspace_1.16.1/11_ADC_continuos_conversion/Inc/uart.h
@@ -0,0 +1,13 @@
+
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "stm32f446xx.h"
+#include <stdint.h>
+void init_uart3_rxtx(void);
+char uart3_read(void);
+void init_uart3_tx(void);
+
+
+#endif /* UART_H_ */
